9c381a89a2aa1c91b8de858c132212d11f3649a7
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-11 20:29:29 +0300

    Add project files.

---
+++ IDistributeCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using IDistributeCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace IDistributeCacheRedisApp.Web.Controllers
+{
+    
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions();
+
+            cacheOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            //_distributedCache.SetString("name", "Ozan",cacheOptions);
+            //await _distributedCache.SetStringAsync("surname" , "Ulus"); 
+            Product product = new Product() { Id = 1, Name = "kalem", Price = 100 };
+            string jsonproduct = JsonConvert.SerializeObject(product);
+
+            //Byte[] byteproduct = Encoding.UTF8.GetBytes(jsonproduct);
+
+            //_distributedCache.Set("product:1", byteproduct);
+
+
+            await _distributedCache.SetStringAsync("product:1", jsonproduct, cacheOptions);
+
+            return View();
+        }
+        public async Task<IActionResult> Show() 
+        {
+            //string name = _distributedCache.GetString("name");
+            //string surname = await _distributedCache.GetStringAsync("surname");
+
+            //   ViewBag.Name = surname;
+
+            string jsonproduct = await _distributedCache.GetStringAsync("product:1");
+
+            //Byte[] byteProduct = _distributedCache.Get("product:1");
+
+            //string jsonproduct = Encoding.UTF8.GetString(byteProduct);
+
+            Product p = JsonConvert.DeserializeObject<Product>(jsonproduct);
+
+            ViewBag.product = p;
+
+            return View();
+        }
+        public IActionResult Delete() 
+        {
+         //_distributedCache.Remove("name");
+          return View();
+        }
+    }
+}
